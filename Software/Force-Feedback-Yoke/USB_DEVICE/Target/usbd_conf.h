0044fcc1f3211fdc0a525e66c2f300d7c09f239b
JennystonJ <56521936+JennystonJ@users.noreply.github.com>
2023-10-06 17:31:54 -0400

    Added report ID (0x01) to report descriptor.

--- Software/Force-Feedback-Yoke/USB_DEVICE/Target/usbd_conf.h
+++ Software/Force-Feedback-Yoke/USB_DEVICE/Target/usbd_conf.h
@@ -74,7 +74,7 @@
 /*---------- -----------*/
 #define USBD_CUSTOMHID_OUTREPORT_BUF_SIZE     4U
 /*---------- -----------*/
-#define USBD_CUSTOM_HID_REPORT_DESC_SIZE     33U
+#define USBD_CUSTOM_HID_REPORT_DESC_SIZE     35U
 /*---------- -----------*/
 #define CUSTOM_HID_FS_BINTERVAL     0x5U
 
