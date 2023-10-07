27215c0a4b6401e8ce5d38add9d020e6690bc0d0
JennystonJ <56521936+JennystonJ@users.noreply.github.com>
2023-10-07 14:57:00 -0400

    Increased out report buffer size to 5 bytes.

--- Software/Force-Feedback-Yoke/USB_DEVICE/Target/usbd_conf.h
+++ Software/Force-Feedback-Yoke/USB_DEVICE/Target/usbd_conf.h
@@ -72,7 +72,7 @@
 /*---------- -----------*/
 #define USBD_SELF_POWERED     1U
 /*---------- -----------*/
-#define USBD_CUSTOMHID_OUTREPORT_BUF_SIZE     4U
+#define USBD_CUSTOMHID_OUTREPORT_BUF_SIZE     5U
 /*---------- -----------*/
 #define USBD_CUSTOM_HID_REPORT_DESC_SIZE     35U
 /*---------- -----------*/
