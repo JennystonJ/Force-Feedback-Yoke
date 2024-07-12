2fc77174e1f5c3038b30a19c400f83bc275621f0
JennystonJ <56521936+JennystonJ@users.noreply.github.com>
2024-07-12 19:02:57 -0400

    Added encoder with unit tests.

---
+++ Software/Force-Feedback-Yoke/Common/Src/encoder.c
@@ -0,0 +1,69 @@
+/*
+ * encoder.c
+ *
+ *  Created on: Mar 17, 2024
+ *      Author: Jennyston
+ */
+
+#include "encoder.h"
+
+void EncoderInit(Encoder_t *encoder, int (*GetEncoderSensorCount)(void)) {
+	encoder->GetEncoderSensorCount = GetEncoderSensorCount;
+
+	encoder->countPerRev = ENCODER_DEFAULT_COUNT_PER_REV;
+	encoder->totalCount = GetEncoderSensorCount();
+	encoder->prevHardCount = encoder->totalCount;
+	encoder->speed = 0;
+}
+
+int EncoderGetCount(Encoder_t *encoder) {
+	return encoder->totalCount;
+}
+
+void EncoderSetCount(Encoder_t *encoder, int count) {
+	encoder->totalCount = count;
+}
+
+void EncoderResetCount(Encoder_t *encoder) {
+	encoder->totalCount = 0;
+}
+
+float EncoderGetSpeed(Encoder_t *encoder) {
+	return encoder->speed;
+}
+
+int EncoderGetCountPerRev(Encoder_t *encoder) {
+	return encoder->countPerRev;
+}
+
+void EncoderUpdate(Encoder_t *encoder, float deltaTimeMs) {
+
+	int deltaCount;
+	int currentHardCount = encoder->GetEncoderSensorCount();
+	int countPerRevHalf = encoder->countPerRev/2;
+
+	// Calculate deltaCount based on current and previous counts
+	if(currentHardCount + countPerRevHalf < encoder->prevHardCount) {
+		// Handle overflow
+		deltaCount = (encoder->countPerRev - encoder->prevHardCount) +
+				currentHardCount;
+	}
+	else if(currentHardCount > encoder->prevHardCount + countPerRevHalf) {
+		// Handle underflow
+		deltaCount = -((encoder->countPerRev - currentHardCount) +
+				encoder->prevHardCount);
+	}
+	else {
+		// default, simply add to total count
+		deltaCount = currentHardCount - encoder->prevHardCount;
+	}
+
+	// Calculate and update speed
+	encoder->speed = deltaCount / deltaTimeMs;
+
+	// Update totalCount
+	encoder->totalCount += deltaCount;
+
+	// Update previous hardware count
+	encoder->prevHardCount = currentHardCount;
+}
