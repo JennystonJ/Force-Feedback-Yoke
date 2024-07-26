e97fb2b2b1c2ebfbb912b02bffee3c3f277e1058
JennystonJ <56521936+JennystonJ@users.noreply.github.com>
2024-07-25 23:08:32 -0400

    Added wrapper for HAL_Delay function.

---
+++ Software/Force-Feedback-Yoke/Core/Inc/delay.h
@@ -0,0 +1,13 @@
+/*
+ * delay.h
+ *
+ *  Created on: Jul 25, 2024
+ *      Author: Jennyston
+ */
+
+#ifndef INC_DELAY_H_
+#define INC_DELAY_H_
+
+void delayMs(int milliseconds);
+
+#endif /* INC_DELAY_H_ */
