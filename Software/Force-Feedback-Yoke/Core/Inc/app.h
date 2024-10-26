4b68bc94a4e8245ce7b90e036cc766ee1be4ce4c
JennystonJ <56521936+JennystonJ@users.noreply.github.com>
2024-10-26 15:28:46 -0400

    Added aileron and elevator conversion values.

--- Software/Force-Feedback-Yoke/Core/Inc/app.h
+++ Software/Force-Feedback-Yoke/Core/Inc/app.h
@@ -8,6 +8,10 @@
 #ifndef INC_APP_H_
 #define INC_APP_H_
 
+//TODO: Move to config
+#define PULLEY_AILERON_ENCODER_STEP_PER_DEGREE (12.0f)
+#define PULLEY_ELEVATOR_ENCODER_STEP_PER_MM ((4096*3)/(3.14159f*19.10f))
+
 void ApplicationInit(void);
 void ApplicationRun(void);
 void ApplicationUpdate(int deltaTimeUs);
