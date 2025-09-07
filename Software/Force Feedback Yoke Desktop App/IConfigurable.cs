de34f5e1ce64bc6a0eb614cf5d8d74bcc1d7b21d
JennystonJ <56521936+JennystonJ@users.noreply.github.com>
2025-09-07 15:36:51 -0400

    Create IConfigurable.cs

---
+++ Software/Force Feedback Yoke Desktop App/IConfigurable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Force_Feedback_Yoke_Desktop_App
+{
+    public interface IConfigurable<TConfig>
+    {
+        public void ApplyConfig(TConfig config);
+        public TConfig ExportConfig();
+    }
+}
