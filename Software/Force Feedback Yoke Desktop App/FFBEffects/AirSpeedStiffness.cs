4f49dbed0ad9f73f384ce418cc63cecc31e03465
JennystonJ <56521936+JennystonJ@users.noreply.github.com>
2025-09-07 15:42:51 -0400

    Added property changed, refined airspeed stiffness calculations.

--- Software/Force Feedback Yoke Desktop App/FFBEffects/AirSpeedStiffness.cs
+++ Software/Force Feedback Yoke Desktop App/FFBEffects/AirSpeedStiffness.cs
@@ -1,21 +1,67 @@
 ﻿using Force_Feedback_Yoke_Desktop_App.FFBForces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Force_Feedback_Yoke_Desktop_App.FFBEffects
 {
-    internal class AirSpeedStiffness : FFBEffect
+    public class AirspeedStiffnessConfig: FFBEffectConfig
     {
-        public double AirSpeed { get; set; }
         public double SpringGain { get; set; }
         public double DamperGain { get; set; }
+    }
+    internal class AirspeedStiffness : FFBEffect
+    {
+        public double Airspeed { get; set; }
+        
+        private double _springGain;
+        public double SpringGain {
+            get => _springGain; 
+            set 
+            { 
+                if (_springGain != value)
+                {
+                    _springGain = value;
+                    OnPropertyChanged(this, new PropertyChangedEventArgs(nameof(SpringGain)));
+                }
+            } 
+        }
+        
+        private double _damperGain;
+        public double DamperGain
+        {
+            get => _damperGain;
+            set
+            {
+                if (_damperGain != value)
+                {
+                    _damperGain = value;
+                    OnPropertyChanged(this, new PropertyChangedEventArgs(nameof(DamperGain)));
+                }
+            }
+        }
 
         private FFBSpring springForce;
         private FFBDamper damperForce;
-        public AirSpeedStiffness(double springGain, double damperGain=0.1)
+        
+        public AirspeedStiffness()
+        {
+            SpringGain = 0;
+            DamperGain = 0.1;
+
+            springForce = new FFBSpring();
+            damperForce = new FFBDamper();
+
+            Forces = [springForce, damperForce];
+
+            // Ensure delegates start disabled
+            Enabled = false;
+            OnEnabledChanged(false);
+        }
+        public AirspeedStiffness(double springGain, double damperGain=0.1)
         {
             SpringGain = springGain;
             DamperGain = damperGain;
@@ -32,12 +78,12 @@ namespace Force_Feedback_Yoke_Desktop_App.FFBEffects
 
         public double AirSpeedStiffnessSpringFunction()
         {
-            return SpringGain * AirSpeed * AirSpeed;
+            return SpringGain * Math.Min(Math.Pow(Airspeed / 100, 2), 1);
         }
 
         public double AirSpeedStiffnessDamperFunction()
         {
-            return DamperGain/1000.0 * AirSpeed;
+            return DamperGain / 1000.0 * Math.Min(Math.Pow(Airspeed / 100, 2), 1);
         }
 
         protected override void OnEnabledChanged(bool enabled)
@@ -59,6 +105,30 @@ namespace Force_Feedback_Yoke_Desktop_App.FFBEffects
                 { "spring_gain", SpringGain },
                 { "damper_gain", DamperGain },
             };
+        }
+
+        public override void ApplyConfig(FFBEffectConfig config)
+        {
+            if (config is AirspeedStiffnessConfig airspeedStiffnessConfig)
+            {
+                SpringGain = airspeedStiffnessConfig.SpringGain;
+                DamperGain = airspeedStiffnessConfig.DamperGain;
+            }
+       
+        }
+
+        public override AirspeedStiffnessConfig ExportConfig()
+        {
+            return new AirspeedStiffnessConfig
+            {
+                SpringGain = this.SpringGain,
+                DamperGain = this.DamperGain,
+            };
+        }
+
+        public override void UpdateData(SimData simData)
+        {
+            Airspeed = simData.AircraftData.airSpeed;
         }
     }
 }
