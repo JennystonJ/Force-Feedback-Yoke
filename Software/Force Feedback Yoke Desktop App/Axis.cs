6502f7b63047155f07e6863fe6094959e66184ad
JennystonJ <56521936+JennystonJ@users.noreply.github.com>
2025-09-10 08:05:00 -0400

    Create Axis.cs

---
+++ Software/Force Feedback Yoke Desktop App/Axis.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Force_Feedback_Yoke_Desktop_App
+{
+    internal enum Axis
+    {
+        Pitch,
+        Roll,
+    }
+}
